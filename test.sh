aa62a6665ef9232d5dd365cadacd478ad6ce986e
mpt62 <134145543+mpt62@users.noreply.github.com>
2023-05-21 10:09:45 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-export
+if [ -d ./target ]; then
+ if [ -f ./target/$APPLICATION_NAME.jar ]; then
+  exit 0
+ else.
+  if [ -f ./target/$APPLICATION_NAME*.jar ]; then
+   mv ./target/$APPLICATION_NAME*.jar ./target/$APPLICATION_NAME.jar
+   exit 0
+  else
+   echo "no $APPLICATION_NAME jar file"
+   exit 1
+  fi
+ fi
+else
+ echo "no directory \"target\""
+ exit 1
+fi
