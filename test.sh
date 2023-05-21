2b96c4fb68e1b7a654ef28e5f04c2ba6f0a9907b
mpt62 <134145543+mpt62@users.noreply.github.com>
2023-05-21 10:19:33 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -d ./target ]; then
- if [ -f ./target/$APPLICATION_NAME.jar ]; then
+ if [ -f ./target/${APPLICATION_NAME}.jar ]; then
   exit 0
  else.
-  if [ -f ./target/$APPLICATION_NAME*.jar ]; then
-   mv ./target/$APPLICATION_NAME*.jar ./target/$APPLICATION_NAME.jar
+  if [ -f ./target/{$APPLICATION_NAME}*.jar ]; then
+   mv ./target/${APPLICATION_NAME}*.jar ./target/${APPLICATION_NAME}.jar
    exit 0
   else
-   echo "no $APPLICATION_NAME jar file"
+   echo "no ${APPLICATION_NAME} jar file"
    exit 1
   fi
  fi
