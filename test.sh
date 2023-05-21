222c186c35a6ccc7b2b10a517466c05d37673688
mpt62 <134145543+mpt62@users.noreply.github.com>
2023-05-21 10:35:51 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 if [ -d ./target ]; then
  echo "dir exists"
- if [ -f ./target/${APPLICATION_NAME}.jar ]; then
+ if [ -f ./target/$APPLICATION_NAME.jar ]; then
   echo "file exists"
   exit 0
  else
-  if [ -f ./target/{$APPLICATION_NAME}*.jar ]; then
+  if [ -f ./target/$APPLICATION_NAME*.jar ]; then
    echo "rename file"
-   mv ./target/${APPLICATION_NAME}*.jar ./target/${APPLICATION_NAME}.jar
+   mv ./target/$APPLICATION_NAME*.jar ./target/${APPLICATION_NAME}.jar
    echo $!
    exit 0
   else
-   echo "no ${APPLICATION_NAME} jar file"
+   echo "no $APPLICATION_NAME jar file"
    exit 1
   fi
  fi
